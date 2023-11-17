da6c6b790efe22d7e176b5f29d0fbf91039b3e88
Allfoy <115825767+Allfoy@users.noreply.github.com>
2023-11-17 16:04:58 +0100

    timed approach; closes 16 secs in closelogging.js

--- js/closelogging.js
+++ js/closelogging.js
@@ -4,12 +4,8 @@ function openWinNh() {
   // Set the URL after opening to avoid opening a blank tab
   newWindow.location.href = 'https://apps.noordhoff.nl/se/home/overview';
 
-  // Event listener for the button click (anchor tag in this case)
-  var anchorTag = document.querySelector('.emotion-cache-166vfo4'); // Replace with the actual class
-  anchorTag.addEventListener('click', function() {
-    // Check if the URL contains '/content/' when the button is clicked
-    if (newWindow.location.href.indexOf("/content/") !== -1) {
-      newWindow.close();
-    }
-  });
+  // Set a timeout to close the window after 16 seconds
+  setTimeout(function() {
+    newWindow.close();
+  }, 16000); // 16 seconds in milliseconds
 }
