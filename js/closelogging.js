51aafd1791da43b5f9cd5e58c1c0894355035c48
Allfoy <115825767+Allfoy@users.noreply.github.com>
2023-11-17 15:26:00 +0100

    added window size to closelogging.js

--- js/closelogging.js
+++ js/closelogging.js
@@ -1,6 +1,6 @@
 function openWinNh() {
   // Open the window with the specified link
-  let newWindow = window.open("https://apps.noordhoff.nl/se/home/overview");
+  let newWindow = window.open('https://apps.noordhoff.nl/se/home/overview', '_blank', 'width=850,height=500,left=350,top=150');
 
   // Check if the URL contains '/content/' and close the window if true
   newWindow.addEventListener("load", function() {
