8dd2cb16990cf98b03b4f49939d186062dc71829
Allfoy <115825767+Allfoy@users.noreply.github.com>
2023-11-17 16:01:34 +0100

    Checking for button closelogging.js

--- js/closelogging.js
+++ js/closelogging.js
@@ -4,10 +4,12 @@ function openWinNh() {
   // Set the URL after opening to avoid opening a blank tab
   newWindow.location.href = 'https://apps.noordhoff.nl/se/home/overview';
 
-  // Check if the URL contains '/content/' when the window is about to unload
-  newWindow.onbeforeunload = function() {
+  // Event listener for the button click (anchor tag in this case)
+  var anchorTag = document.querySelector('.emotion-cache-166vfo4'); // Replace with the actual class
+  anchorTag.addEventListener('click', function() {
+    // Check if the URL contains '/content/' when the button is clicked
     if (newWindow.location.href.indexOf("/content/") !== -1) {
       newWindow.close();
     }
-  };
+  });
 }
