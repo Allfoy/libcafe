1b546ac6efbc45413462711719a4f7f92b67ef43
Allfoy <115825767+Allfoy@users.noreply.github.com>
2023-11-17 15:42:50 +0100

    changed includes to IndexOf in closelogging.js

--- js/closelogging.js
+++ js/closelogging.js
@@ -1,12 +1,12 @@
 function openWinNh() {
-  let newWindow = window.open('', '_blank', 'width=850,height=500,left=350,top=150');
+  var newWindow = window.open('', '_blank', 'width=850,height=500,left=350,top=150');
 
   // Set the URL after opening to avoid opening a blank tab
   newWindow.location.href = 'https://apps.noordhoff.nl/se/home/overview';
 
   // Check if the URL contains '/content/' and close the window if true
   newWindow.addEventListener("load", function() {
-    if (newWindow.location.href.includes("/content/")) {
+    if (newWindow.location.href.indexOf("/content/") !== -1) {
       newWindow.close();
     }
   });
