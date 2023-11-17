1f01c0fb98f71784acfcee42f117bcee98a8d001
Allfoy <115825767+Allfoy@users.noreply.github.com>
2023-11-17 16:08:36 +0100

    sends console every second closelogging.js

--- js/closelogging.js
+++ js/closelogging.js
@@ -4,8 +4,14 @@ function openWinNh() {
   // Set the URL after opening to avoid opening a blank tab
   newWindow.location.href = 'https://apps.noordhoff.nl/se/home/overview';
 
-  // Set a timeout to close the window after 16 seconds
-  setTimeout(function() {
-    newWindow.close();
-  }, 16000); // 16 seconds in milliseconds
+  // Set an interval to send a message to the original window every second
+  var interval = setInterval(function() {
+    if (newWindow.location.href.indexOf("/content/") !== -1) {
+      clearInterval(interval); // Stop the interval if the condition is met
+      newWindow.close();
+    } else {
+      // Send a message to the console of the original window
+      window.opener.console.log("Checking the URL...");
+    }
+  }, 1000); // Every second
 }
