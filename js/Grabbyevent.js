9fc9bed0328bc3d4123d6500416f1a5687eb5f52
Allfi Jabiullah <allfi09@hotmail.com>
2024-01-25 16:40:09 +0100

    added english keyword

--- js/Grabbyevent.js
+++ js/Grabbyevent.js
@@ -4,7 +4,8 @@ const calendarId = 'kvme0ikmjq4825g8ee860tm058clorcg@import.calendar.google.com'
 
 // Object with keywords and their respective links
 const keywordLinks = {
-    'nat': 'https://e-book.boomdigitaal.nl/boek/9789464420180?layoutmode-double=1/sso',
+'entl':'https://apps.noordhoff.nl/se/content/book/4e33ffb8-40c9-45fa-a67c-eae545d7dbea/ebooks/d6cafc0b-fb70-438c-8668-c017ee87ff73',
+'nat': 'https://e-book.boomdigitaal.nl/boek/9789464420180?layoutmode-double=1/sso',
 'schk': 'https://apps.noordhoff.nl/se/content/book/3d237f12-c196-4650-839c-bea7f798792e/ebooks/3847b67a-6f0b-4d10-9f5e-d485d7496fda',
 'biol': 'https://apps.noordhoff.nl/se/content/book/3c9f39f7-6a4b-4403-a898-508d1ddcf68f/ebooks/2afd6210-0fb1-4007-8d2a-b63ef7cad649',
 'netl': 'https://apps.noordhoff.nl/se/content/book/a904673b-f07d-480a-8201-57b56d94112b/ebooks/89f0e06c-3f93-403f-9804-04bad7a197de',
