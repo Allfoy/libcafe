3f85935bb3122b3b19cfb7bbc6c1c67bf5c109f2
Allfi Jabiullah <allfi09@hotmail.com>
2024-02-08 13:02:10 +0100

    tryna add hidey

--- js/Toggler.js
+++ js/Toggler.js
@@ -2,3 +2,8 @@ function toggleIframe(iframeId) {
     var iframe = document.getElementById(iframeId);
     iframe.style.display = (iframe.style.display === 'none') ? 'flex' : 'none';
 }
+
+function hidey(tabb){
+    var bbat = document.getElementsByClassName(tabb);
+    bbat.style.left = -500; // Changes the left property of the asked elementclass
+}
