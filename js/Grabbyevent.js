b9a6e5d06cbdec02adf57f19cd442a9ef89d4110
Allfi Jabiullah <allfi09@hotmail.com>
2024-09-03 12:00:44 +0200

    logic mistake through !undefined === true

--- js/Grabbyevent.js
+++ js/Grabbyevent.js
@@ -262,8 +262,8 @@ function fetchEventsAndUpdateTime(calid) {
                     "12:20": "12:45",
                     "14:15": "14:30"
                 };
-                let schoolStart =  filteredEvents[0]?.start.dateTime == !undefined ? filteredEvents[0]?.start.dateTime : '0' ;
-                let schoolEnd = filteredEvents[filteredEvents.length-1]?.end.dateTime == !undefined ? filteredEvents[filteredEvents.length-1]?.end.dateTime : '01/01/1970 23:59:59' ;
+                let schoolStart =  filteredEvents[0]?.start.dateTime !== undefined ? filteredEvents[0]?.start.dateTime : '0' ;
+                let schoolEnd = filteredEvents[filteredEvents.length-1]?.end.dateTime !== undefined ? filteredEvents[filteredEvents.length-1]?.end.dateTime : '01/01/1970 23:59:59' ;
                 const conditions = {//put the conditions in an object literal for readability
                     weekend: new Date().getDay() === 6 || new Date().getDay() === 0, //is it weekend?
                     isInBreak: Object.entries(breaks).some(([start, end]) => {return currentTime >= start && currentTime <= end;}), // is it break?
